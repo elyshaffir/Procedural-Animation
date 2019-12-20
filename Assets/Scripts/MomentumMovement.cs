e1f37bbd34a09ff6232b0b288863fd1ccfcb3b70
Ely Shaffir <elyshaffir@gmail.com>
2019-12-20 19:22:27 +0200

    Basic Non-Functional
    Can move and preserve momentum properly, can't climb ramps and is hard to control.

--- Assets/Scripts/MomentumMovement.cs
+++ Assets/Scripts/MomentumMovement.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+
 
 class MomentumMovement : MonoBehaviour
 {
+
+    const float MOVEMENT_SPEED_LIMIT = 30;
+
     public float movementSpeed;
     public float rotationSpeed;
     public float jumpForce;
@@ -9,7 +14,12 @@ class MomentumMovement : MonoBehaviour
     Rigidbody body;
     float vertical;
     float horizontal;
-    bool isGrounded;
+    HashSet<GameObject> environmentTouching;
+
+    void Awake()
+    {
+        environmentTouching = new HashSet<GameObject>();
+    }
 
     void Start()
     {
@@ -20,32 +30,43 @@ class MomentumMovement : MonoBehaviour
     {
         vertical = Input.GetAxis("Vertical");
         horizontal = Input.GetAxis("Horizontal");
-        if (Input.GetAxis("Jump") > 0)
+        if (environmentTouching.Count > 0)
         {
-            if (isGrounded)
+            if (Input.GetAxis("Jump") > 0)
             {
                 body.AddForce(transform.up * jumpForce);
             }
+            Vector3 velocity = (transform.forward * vertical) * movementSpeed * Time.fixedDeltaTime;
+            velocity.y = body.velocity.y;
+            body.velocity += velocity;
         }
-        Vector3 velocity = (transform.forward * vertical) * movementSpeed * Time.fixedDeltaTime;
-        velocity.y = body.velocity.y;
-        body.velocity = velocity;
-        transform.Rotate((transform.up * horizontal) * rotationSpeed * Time.fixedDeltaTime);
+        Vector3 angularVelocity = (transform.up * horizontal) * rotationSpeed * Time.fixedDeltaTime;
+        angularVelocity.x = body.angularVelocity.y;
+        angularVelocity.z = body.angularVelocity.z;
+        body.angularVelocity += angularVelocity;
+        if (body.velocity.magnitude > MOVEMENT_SPEED_LIMIT)
+        {
+            body.velocity = Vector3.ClampMagnitude(body.velocity, MOVEMENT_SPEED_LIMIT);
+        }
+        if (body.angularVelocity.magnitude > MOVEMENT_SPEED_LIMIT)
+        {
+            body.angularVelocity = Vector3.ClampMagnitude(body.angularVelocity, MOVEMENT_SPEED_LIMIT);
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.root.tag == ("Ground"))
+        if (collision.transform.root.tag == ("Environment"))
         {
-            isGrounded = true;
+            environmentTouching.Add(collision.gameObject);
         }
     }
 
     void OnCollisionExit(Collision collision)
     {
-        if (collision.transform.root.tag == ("Ground"))
+        if (collision.transform.root.tag == ("Environment"))
         {
-            isGrounded = false;
+            environmentTouching.Remove(collision.gameObject);
         }
     }
 }
