27a59d067c37dc72cbef2d201049802b35f50b04
Ely Shaffir <elyshaffir@gmail.com>
2019-12-21 17:30:02 +0200

    Basic Functional
    Can climb and move, not jump.

--- Assets/Scripts/MomentumMovement.cs
+++ Assets/Scripts/MomentumMovement.cs
@@ -1,72 +1,24 @@
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-
+[RequireComponent(typeof(CharacterController))]
 class MomentumMovement : MonoBehaviour
 {
+    const float Speed = 400f;
 
-    const float MOVEMENT_SPEED_LIMIT = 30;
+    public GameObject playerCamera;
 
-    public float movementSpeed;
-    public float rotationSpeed;
-    public float jumpForce;
-
-    Rigidbody body;
-    float vertical;
-    float horizontal;
-    HashSet<GameObject> environmentTouching;
-
-    void Awake()
-    {
-        environmentTouching = new HashSet<GameObject>();
-    }
+    CharacterController controller;
 
     void Start()
     {
-        body = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        vertical = Input.GetAxis("Vertical");
-        horizontal = Input.GetAxis("Horizontal");
-        if (environmentTouching.Count > 0)
-        {
-            if (Input.GetAxis("Jump") > 0)
-            {
-                body.AddForce(transform.up * jumpForce);
-            }
-            Vector3 velocity = (transform.forward * vertical) * movementSpeed * Time.fixedDeltaTime;
-            velocity.y = body.velocity.y;
-            body.velocity += velocity;
-        }
-        Vector3 angularVelocity = (transform.up * horizontal) * rotationSpeed * Time.fixedDeltaTime;
-        angularVelocity.x = body.angularVelocity.y;
-        angularVelocity.z = body.angularVelocity.z;
-        body.angularVelocity += angularVelocity;
-        if (body.velocity.magnitude > MOVEMENT_SPEED_LIMIT)
-        {
-            body.velocity = Vector3.ClampMagnitude(body.velocity, MOVEMENT_SPEED_LIMIT);
-        }
-        if (body.angularVelocity.magnitude > MOVEMENT_SPEED_LIMIT)
-        {
-            body.angularVelocity = Vector3.ClampMagnitude(body.angularVelocity, MOVEMENT_SPEED_LIMIT);
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.root.tag == ("Environment"))
-        {
-            environmentTouching.Add(collision.gameObject);
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.transform.root.tag == ("Environment"))
-        {
-            environmentTouching.Remove(collision.gameObject);
-        }
+        Vector3 moveVector = playerCamera.transform.forward * Input.GetAxis("Vertical");
+        moveVector += playerCamera.transform.right * Input.GetAxis("Horizontal");
+        moveVector *= Speed * Time.deltaTime;
+        controller.SimpleMove(moveVector);
     }
 }
