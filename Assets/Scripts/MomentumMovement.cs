8a083e24fd0edbeefdcd20885bd4979d139ecabe
Ely Shaffir <elyshaffir@gmail.com>
2019-12-22 22:29:55 +0200

    Velocity Rotation Added

--- Assets/Scripts/MomentumMovement.cs
+++ Assets/Scripts/MomentumMovement.cs
@@ -13,11 +13,27 @@ class MomentumMovement : MonoBehaviour
         controller = GetComponent<CharacterController>();
     }
 
-    void Update()
+    void MoveAccordingToCamera()
     {
         Vector3 moveVector = playerCamera.transform.forward * Input.GetAxis("Vertical");
         moveVector += playerCamera.transform.right * Input.GetAxis("Horizontal");
         moveVector *= speed * Time.deltaTime;
         controller.SimpleMove(moveVector);
+    }
+
+    void RotateAccordingToVelocity()
+    {
+        Vector3 lookAt = transform.position + controller.velocity.normalized;
+        Vector3 targetPostition = new Vector3(lookAt.x, transform.position.y, lookAt.z);
+        // transform.LookAt(targetPostition);
+        Quaternion q = Quaternion.LookRotation(targetPostition - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 500 * Time.deltaTime);
+
+    }
+
+    void Update()
+    {
+        MoveAccordingToCamera();
+        RotateAccordingToVelocity();
     }
 }
