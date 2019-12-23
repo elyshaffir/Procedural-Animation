8dd30afc77c9470668d81462950897ae99140ec4
Ely Shaffir <elyshaffir@gmail.com>
2019-12-23 16:53:33 +0200

    Speed Change Bug Fixed

--- Assets/Scripts/MomentumMovement.cs
+++ Assets/Scripts/MomentumMovement.cs
@@ -6,28 +6,40 @@ class MomentumMovement : MonoBehaviour
     public GameObject playerCamera;
 
     CharacterController controller;
-    float speed = 400f;
+    float speed = 200f;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
+    Vector3 Sign(Vector3 vector)
+    {
+        return new Vector3(
+            Mathf.Sign(vector.x),
+            Mathf.Sign(vector.y),
+            Mathf.Sign(vector.z)
+        );
+    }
+
     void MoveAccordingToCamera()
     {
-        Vector3 moveVector = playerCamera.transform.forward * Input.GetAxis("Vertical");
-        moveVector += playerCamera.transform.right * Input.GetAxis("Horizontal");
+        Vector3 moveVector = Sign(playerCamera.transform.forward) * Input.GetAxis("Vertical");
+        moveVector += Sign(playerCamera.transform.right) * Input.GetAxis("Horizontal");
         moveVector *= speed * Time.deltaTime;
         controller.SimpleMove(moveVector);
+        Debug.Log(playerCamera.transform.forward);
     }
 
     void RotateAccordingToVelocity()
     {
         Vector3 lookAt = transform.position + controller.velocity.normalized;
         Vector3 targetPostition = new Vector3(lookAt.x, transform.position.y, lookAt.z);
-        // transform.LookAt(targetPostition);
-        Quaternion q = Quaternion.LookRotation(targetPostition - transform.position);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 500 * Time.deltaTime);
+        if (targetPostition - transform.position != Vector3.zero)
+        {
+            Quaternion q = Quaternion.LookRotation(targetPostition - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 500 * Time.deltaTime);
+        }
 
     }
 
