08eced0b53ec7d2ddd85ef8875130ded7ae261eb
Ely Shaffir <elyshaffir@gmail.com>
2019-12-25 18:31:28 +0200

    Acceleration Tilt Added
    Acceleration tilt is inverted in some directions, there are spikes in the rotation from time to time.

--- Assets/Scripts/MomentumMovement.cs
+++ Assets/Scripts/MomentumMovement.cs
@@ -7,10 +7,12 @@ class MomentumMovement : MonoBehaviour
 
     CharacterController controller;
     float speed = 400f;
+    Vector3 lastVelocity;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        lastVelocity = controller.velocity;
     }
 
     Vector3 ScaleDirectionVector(Vector3 direction)
@@ -23,7 +25,7 @@ class MomentumMovement : MonoBehaviour
         );
     }
 
-    void MoveAccordingToCamera()
+    void Move()
     {
         Vector3 moveVector = ScaleDirectionVector(playerCamera.transform.forward) * Input.GetAxis("Vertical");
         moveVector += ScaleDirectionVector(playerCamera.transform.right) * Input.GetAxis("Horizontal");
@@ -31,7 +33,7 @@ class MomentumMovement : MonoBehaviour
         controller.SimpleMove(moveVector);
     }
 
-    void RotateAccordingToVelocity()
+    void RotateToVelocity()
     {
         Vector3 lookAt = transform.position + controller.velocity.normalized;
         Vector3 targetPostition = new Vector3(lookAt.x, transform.position.y, lookAt.z);
@@ -43,9 +45,23 @@ class MomentumMovement : MonoBehaviour
 
     }
 
+    void TiltToAcceleration()
+    {
+        // WHENEVER HE ACCELERATES it tilts in that direction!
+        // When he collides face first into the wall, it tilts backwards
+
+        Vector3 centerOfMass = controller.center + controller.transform.position;
+        Vector3 acceleration = controller.velocity / Time.deltaTime - lastVelocity;
+        Vector3 tilt = new Vector3(acceleration.z, 0, acceleration.x) / 3; // Rotation flipped in some directions        
+        Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles + tilt);
+        controller.transform.GetChild(0).rotation = Quaternion.Lerp(controller.transform.GetChild(0).rotation, targetRotation, 10 * Time.deltaTime);
+    }
+
     void Update()
     {
-        MoveAccordingToCamera();
-        RotateAccordingToVelocity();
+        Move();
+        RotateToVelocity();
+        TiltToAcceleration();
+        lastVelocity = controller.velocity / Time.deltaTime;
     }
 }
