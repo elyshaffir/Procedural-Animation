cc8303bf9e619c7a732d1788508a94107a91220e
Ely Shaffir <elyshaffir@gmail.com>
2020-01-15 23:51:31 +0200

    Constants added, momentum problem reported.

--- Assets/Scripts/MomentumMovement.cs
+++ Assets/Scripts/MomentumMovement.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 class MomentumMovement : MonoBehaviour
 {
+
+    /*
+        Can chage direction very easily, not very realistic.
+    */
+
+    const float MovementSpeed = 500f;
+    const float RotationSpeed = 5;
+    const float TiltAngle = 20;
+
     public GameObject playerCamera;
     public GameObject playerModel;
 
     CharacterController controller;
-    float speed = 400f;
     Vector3 lastVelocity;
 
     void Start()
@@ -22,7 +30,7 @@ class MomentumMovement : MonoBehaviour
             direction.x,
             0,
             direction.z
-        ).normalized * speed * Time.deltaTime;
+        ).normalized * MovementSpeed * Time.deltaTime;
     }
 
     void Move()
@@ -39,7 +47,7 @@ class MomentumMovement : MonoBehaviour
         if (targetPosition - transform.position != Vector3.zero)
         {
             Quaternion q = Quaternion.LookRotation(targetPosition - transform.position);
-            transform.rotation = Quaternion.Lerp(transform.rotation, q, 10 * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, q, RotationSpeed * Time.deltaTime);
         }
 
     }
@@ -48,7 +56,7 @@ class MomentumMovement : MonoBehaviour
     {
         acceleration.y = 0;
         Vector3 tiltAxis = Vector3.Cross(acceleration, Vector3.up);
-        float angle = Mathf.Clamp(-acceleration.magnitude * 2, -20, 20);
+        float angle = Mathf.Clamp(-acceleration.magnitude / 2, -TiltAngle, TiltAngle);
         Quaternion targetRotation = Quaternion.AngleAxis(angle, tiltAxis) * transform.rotation;
         return targetRotation.eulerAngles;
     }
@@ -58,7 +66,7 @@ class MomentumMovement : MonoBehaviour
         Vector3 acceleration = (controller.velocity - lastVelocity) / Time.deltaTime;
         Vector3 tilt = CalculateTilt(acceleration);
         Quaternion targetRotation = Quaternion.Euler(tilt);
-        playerModel.transform.rotation = Quaternion.Lerp(playerModel.transform.rotation, targetRotation, 10 * Time.deltaTime);
+        playerModel.transform.rotation = Quaternion.Lerp(playerModel.transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
     }
 
     void FixedUpdate()
