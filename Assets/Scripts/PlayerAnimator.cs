7bf3282d8b040d741afbc9f7dac1e8f0d2cc4a19
Ely Shaffir <elyshaffir@gmail.com>
2020-01-20 23:49:08 +0200

    Temporary keyframes, always running

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    float progress;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        progress += .05f;
+        if (progress >= 1)
+        {
+            progress = 0;
+            Debug.Log("RESET");
+        }
+        animator.SetFloat("RunProgress", progress);
+    }
+}
