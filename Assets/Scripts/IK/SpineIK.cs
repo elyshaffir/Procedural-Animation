52721d83127331004abe36f75e0817886a982069
Ely Shaffir <elyshaffir@gmail.com>
2020-02-27 10:36:54 +0200

    Spine IK functional

--- Assets/Scripts/IK/SpineIK.cs
+++ Assets/Scripts/IK/SpineIK.cs
@@ -4,16 +4,18 @@ namespace ProceduralAnimation.IK
 {
     class SpineIK : IKBehaviour
     {
+        Vector3 lastTargetPosition; // move the IK by the difference in distance from the last frame, add to the position -- dont override it.
+
+        void Start()
+        {
+            lastTargetPosition = target.position;
+        }
+
+
         void LateUpdate()
         {
-            // works only without animator
-            // if you put another child under "spine" and attatch to it the HandIK script, it works in the way where
-            // the more you offset the child from the original "spine" it affects the model more, and works properly with animator.
-            // -- but: the movement is also extremely weird.
-            // IKs that affect each other work. for example, a HandIK on spine6 with chainLength 6 works just fine with hand and leg IKs.
-            // HandIK on spine with chainLength 1 works, but in a weird way as well.            
             transform.position = Vector3.Lerp(transform.position, target.position, snapBackStrength);
-            // transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, snapBackStrength);
+            transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, snapBackStrength);
         }
     }
 }
