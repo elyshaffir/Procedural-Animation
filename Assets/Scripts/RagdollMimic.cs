14bcac99fbec5e54d092e633f6167d204165fff2
Ely Shaffir <elyshaffir@gmail.com>
2020-03-05 14:29:27 +0200

    Ragdoll done - needs some refinement.

--- Assets/Scripts/RagdollMimic.cs
+++ Assets/Scripts/RagdollMimic.cs
@@ -6,26 +6,18 @@ namespace ProceduralAnimation
     {
         /*
         * We need to unite models into one since pose matching is a thing, unless it can be bypassed
-            -   Pose matching could be done by trying to interpolate AFTER the physics affect the limb
-                --  if this can be done, we can probably get rid of the playerTransitionModel
-                --  the same tecnique could be applied to both animations and keyframes
-                    --- provided that we can find a way to have the animation still run while the animated model is invisible
+            -   Pose matching could be done by trying to interpolate AFTER the physics affect the limb            
+                --  the same tecnique could be applied to both animations and keyframes                    
                 --  if this works for animation, it is possible to leave it always in a state of animation matching, this way the model
                     will be interactive with physics
-            -   Bone softness could be implemented by the speed at which the bone interpolates to the position of it in animation
-
-        * When pressing H without having the ragdoll active, it could be a clue to the animation matching
-            - The one model that will be seen by the user is that mimicking ragdoll, while there is another invisible model being animated 
-                -- IK will affect the invisible animated model
-
-        * Remember to move only the bones that are joints
-
-        * Must unite all models into one, it's an ugly headache to solve the re-positionning of every model on each transition        
+            -   Bone softness could be implemented by overshooting to the position of it in animation 
+				-- using the MoveTowards function is possible
         */
         const float LerpTime = 10f;
 
         public GameObject mimicModel;
         public GameObject animatedModel;
+        public Transform mimicObjectToFollow;
 
         bool physicsActive = false;
 
@@ -39,6 +31,10 @@ namespace ProceduralAnimation
             if (!physicsActive)
             {
                 Mimic(animatedModel.transform, mimicModel.transform);
+            }
+            else
+            {
+                animatedModel.transform.position = mimicObjectToFollow.position;
             }
             if (Input.GetKeyDown(KeyCode.G))
             {
@@ -73,9 +69,12 @@ namespace ProceduralAnimation
 
         static void Mimic(Transform currentAnimated, Transform currentMimic)
         {
-            currentMimic.position = Vector3.Lerp(currentMimic.position, currentAnimated.position, LerpTime * Time.deltaTime);
-            currentMimic.rotation = Quaternion.Lerp(currentMimic.rotation, currentAnimated.rotation, LerpTime * Time.deltaTime);
-
+            Rigidbody rb;
+            if (currentMimic.TryGetComponent<Rigidbody>(out rb) || currentMimic.name == "Player Ragdoll") // Nasty
+            {
+                currentMimic.position = Vector3.Lerp(currentMimic.position, currentAnimated.position, LerpTime * Time.deltaTime);
+                currentMimic.rotation = Quaternion.Lerp(currentMimic.rotation, currentAnimated.rotation, LerpTime * Time.deltaTime);
+            }
             /*
                 This loop assumes that if a currentAnimated were to have more children than currentMimic,
                 they were irrelevant for the mimicking process.
